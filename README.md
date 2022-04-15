7ec433b5372b0ed463de2a68bb87becc5f385381
m8xp0w3r <nilsleistner@me.com>
2022-04-15 21:38:02 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LatschiSwiftUIImagePicker
+
+A description of this package.
---
+++ Sources/LatschiSwiftUIImagePicker/LatschiSwiftUIImagePicker.swift
@@ -0,0 +1,6 @@
+public struct LatschiSwiftUIImagePicker {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
